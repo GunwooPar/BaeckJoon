98a20aaa05387dfb508011ba3a45f2f638e51f40
GunWoo Park <rjsdn7895@gnu.ac.kr>
2025-08-12 00:57:02 +0900

    [Silver III] Title: N과 M (7), Time: 756 ms, Memory: 129284 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.py
@@ -0,0 +1,24 @@
+import sys
+
+def input():
+    return sys.stdin.readline().rstrip()
+
+def dfs(curr_sequence, number_list):
+    if len(curr_sequence) == M:
+        print(*curr_sequence)
+        return 0
+
+    for number in sorted(number_list):
+        curr_sequence.append(number)
+        dfs(curr_sequence, number_list)
+        curr_sequence.pop()
+
+
+N, M = map(int, input().split())
+
+
+number_list = list(map(int,input().split()))
+
+sequence = []
+
+dfs(sequence, number_list)
